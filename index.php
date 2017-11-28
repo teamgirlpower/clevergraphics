c61b5edecbd2d9c0821292cca78f602b76c8b158
Sonja <sonja.cederlund@edu.turkuamk.fi>
2017-11-28 13:30:40 +0200

    PHP still has some issues

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <?php
     $name = $_POST['name'];
     $title = $_POST['title'];
-    $subject = $_POST['subject'];
-    $from = 'From: CleverGraphics'; 
-    $to = 'sonja.cederlund@icloud.com'; 
+    $message = $_POST['message'];
+    $from = 'From: CleverGraphics';
+    $to = 'sonja.cederlund@icloud.com';
     $subject = 'Customer Contact on Clever Graphics Site';
+    $human = $_POST['human'];
+
 
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
-				
-    if ($_POST['submit'] && $human == '4') {				 
-        if (mail ($to, $subject, $body, $from)) { 
-	    echo '<p>Your message has been sent!</p>';
-	} else { 
-	    echo '<p>Something went wrong, go back and try again!</p>'; 
-	} 
-    } else if ($_POST['submit'] && $human != '4') {
-	echo '<p>You answered the anti-spam question incorrectly!</p>';
+
+if ($_POST['submit'] && $human == '4') {
+    if (mail($to, $subject, $body, $from)) {
+        echo'<p>Your message has been sent!</p>';
+    } else {
+        echo'<p>Something went wrong, go back and try again!</p>';
     }
-?>
+} elseif ($_POST['submit'] && $human != '4') {
+    echo'<p>You answered the anti-spam question incorrectly!</p>';
+}
